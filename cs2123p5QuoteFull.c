f62c091e356c6297d756de2c206868f239ab59b3
distractor2004 <distractor2004@gmail.com>
2015-11-24 13:48:56 -0600

    works up to quote errors for now
    Works up to quote errors, will continue working on error checks with quotes and with Define errors in driver

--- cs2123p5QuoteFull.c
+++ cs2123p5QuoteFull.c
@@ -22,26 +22,55 @@ Notes:
 **************************************************************************/
 QuoteResult determineQuote(Tree tree, QuoteSelection quoteSelection)
 {
-    QuoteResult *newQuote;
-    NodeT *kid;
-    NodeT *kkid;
-    int i,k;
-      for (i = 0; i < quoteSelection->iQuoteItemCnt; i++)
-      {
-        kid = findId(tree->pRoot, quoteSelection->quoteItemM[i].szOptionId);
-        kkid = kid->pChild;
-          for (k = 1; k <= quoteSelection->quoteItemM[i].iSelection; k++)
-          {
-            if (kkid != NULL)
-            {
-              //newQuote->error = quoteSelection->quoteItemM[i];
-              break;
-            }
-            else
-              kkid = kkid->pSibling;
-          }
+    QuoteResult newQuote;
+    newQuote.dTotalCost = 0.0;
 
-      }
-      printQuote(kkid, kid);
-      return *newQuote;
+    NodeT *pKid;
+    int q, i;
+    //i count is the iLevel, q is the count of the queue
+
+    for(q = 0; q < quoteSelection->iQuoteItemCnt; q++)
+    {
+        /*
+            * This is a complete quote
+            *
+            QUOTE BEGIN
+            QUOTE OPTION 0 model 1
+            QUOTE OPTION 1 engine_base 1
+            QUOTE OPTION 1 color_base 2
+            QUOTE OPTION 1 audio_base 2
+            QUOTE OPTION 0 warranty 2
+            QUOTE END
+            *
+            *
+            ********************************* PARTIAL QUOTE ********
+            *
+            QUOTE BEGIN
+            QUOTE OPTION 0 model 3
+            QUOTE OPTION 1 engine_oy 2
+            QUOTE OPTION 1 color_oy 2
+            QUOTE OPTION 0 warranty 3
+            QUOTE END
+            *
+        */
+        //using findId to find parent node szOptionId
+        pKid = findId(tree->pRoot,quoteSelection->quoteItemM[q].szOptionId);
+        //Print title of the Parent Node
+        printf("\n%s\t", pKid->element.szTitle);
+        //move to options below parent node for iSelection
+        pKid = pKid->pChild;
+        //traverse siblings based on iSelection, starting at 1
+        for(i = 1; i < quoteSelection->quoteItemM[q].iSelection; i++)
+            pKid = pKid->pSibling;
+
+        printf("\t%s", pKid->element.szTitle);
+        printf("\t\t\t%.2lf\n",  pKid->element.dCost);
+        newQuote.dTotalCost += pKid->element.dCost;
+
+    }
+    if(quoteSelection->iQuoteItemCnt < 5)
+        newQuote.returnCode = 1;
+    else
+         newQuote.returnCode = 0;
+      return newQuote;
 }
